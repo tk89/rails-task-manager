a28b05e2b6c8cffcc0d90a1f4ce7bea9644320e2
Tasneem Karwa <tasneemkarwa@gmail.com>
2019-02-19 17:24:27 +0000

    to do list-working

--- db/migrate/20190219141527_create_tasks.rb
+++ db/migrate/20190219141527_create_tasks.rb
@@ -3,7 +3,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     create_table :tasks do |t|
       t.string :title
       t.text :details
-      t.boolean :completed, default: false
+      t.boolean :completed
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_120127) do
+ActiveRecord::Schema.define(version: 2019_02_19_141527) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed", default: false
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
